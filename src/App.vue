5c35a378637b5bd495cc869c0b800b0751567512
Alima <alima@softservice.org>
2022-05-26 14:32:45 +0400

    Init project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Stream />
+</template>
+
+<script>
+import Stream from "@/components/Stream.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Stream,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
